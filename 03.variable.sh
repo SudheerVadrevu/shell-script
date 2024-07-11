0c6328c2c5d04efd5e2d0d887014cb420808b70b
Sudheer Vadrevu <vadrevusudheer@gmail.com>
2024-07-11 14:01:50 +0200

    hello script

---
+++ 03.variable.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+
+Person1=$1
+Person2=$2
+
+echo " Hi ra $Person1, ala vunnavu"
+echo "Hi, ra $Person2 nenu bane vunnanu nuvu emi chesthunavu"
+echo "Job chesthuna, nuvu inka kale yena $Person1"
+echo "ha avunu ra $Person2"
