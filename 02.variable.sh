fa361ca478bf763546ac42435a53dd44f2a489a2
Sudheer Vadrevu <vadrevusudheer@gmail.com>
2024-07-11 13:39:04 +0200

    hello script

--- 02.variable.sh
+++ 02.variable.sh
@@ -4,6 +4,6 @@ Person1=Ravi
 Person2=sidhu
 
 echo " Hi ra $Person1, ala vunnavu"
-echo "Hi, ra $person2 nenu bane vunnanu nuvu emi chesthunavu"
-echo "Job chesthuna, nuvu inka kale yena $person1"
-echo "ha avunu ra $person2"
+echo "Hi, ra $Person2 nenu bane vunnanu nuvu emi chesthunavu"
+echo "Job chesthuna, nuvu inka kale yena $Person1"
+echo "ha avunu ra $Person2"
