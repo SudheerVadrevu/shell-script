276c0b3236d3102c02eaa59a7d4592d1692c7f52
Sudheer Vadrevu <vadrevusudheer@gmail.com>
2024-07-11 17:08:52 +0200

    hello script

---
+++ if-condition.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+
+Sudheer=$1
+if [ $Sudheer -gt 30 ]
+then
+    echo "given number $Sudheer is greater number"
+else
+    echo "given number $Sudheer is lesser than existion number"
+fi
