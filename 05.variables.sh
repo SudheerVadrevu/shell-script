6eea39f3ab7255bf8f86256f6a59153c100a3e04
Sudheer Vadrevu <vadrevusudheer@gmail.com>
2024-07-11 15:55:59 +0200

    hello script

---
+++ 05.variables.sh
@@ -0,0 +1,6 @@
+# !/bin/bash
+
+Bikes=(chethak,splender,passion+,pulsar)
+echo "the name of the first bike is ${Bikes[0]}"
+echo "the name of the second bike is ${Bikes[1]}"
+echo "the name of the all bikes is ${Bikes[@]}"
