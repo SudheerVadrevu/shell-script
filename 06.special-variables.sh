b385ad1d37462c77ed374b0baa8d3c8e000ea1bc
Sudheer Vadrevu <vadrevusudheer@gmail.com>
2024-07-11 16:47:04 +0200

    hello script

--- 06.special-variables.sh
+++ 06.special-variables.sh
@@ -5,3 +5,5 @@ echo "number of variables shown: $#"
 echo "name of the script $0"
 echo "The present working directory $PWD"
 echo "the home directory is: $HOME"
+echo "Which user is wriiting this script $USER"
+
