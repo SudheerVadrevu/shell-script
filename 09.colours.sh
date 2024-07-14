6b223fd4bb06022f5b7f6b61d3a51db3cfd2c0b7
Sudheer Vadrevu <vadrevusudheer@gmail.com>
2024-07-14 17:37:56 +0200

    hello-script

---
+++ 09.colours.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+dog=$(id -u)
+DATE=$(date +%F-%H-%M-%S)
+SCRIPT=$(echo $0 | cut -d "." -f2) #"echo $0" means current name of the script and when we run the command 
+                                   #inside the script we have to give inside (...)
+LOGS=/tmp/$SCRIPT-$DATE.log
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+echo "Script started executing at: $TIMESTAMP"
+valid() {   
+
+    if [ $1 -ne 0 ]   
+    then
+        echo " $2  $R Failure $N" 
+        exit 1
+    else
+        echo "$2 is $G Success $N"
+    fi
+         
+
+}
+
+if [ $dog -ne 0 ]
+then
+    echo "it is not an super user or root user"
+    exit 1
+fi
+  
+dnf install mysql -y &>>$LOGS
+valid $? "Installing mysql" 
+
+dnf install git -y &>>$LOGS
+valid $? "Installing git" 
+
+dnf install rubbbb -y &>>$LOGS
+valid $? "Installing git" 
