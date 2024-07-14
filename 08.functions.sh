29d112d776cfdc9988522ca313b8b0c5dd730715
Sudheer Vadrevu <vadrevusudheer@gmail.com>
2024-07-14 17:49:02 +0200

    hello-script

--- 08.functions.sh
+++ 08.functions.sh
@@ -15,10 +15,10 @@ valid() {
 
     if [ $1 -ne 0 ]   
     then
-        echo " $2  $R Failure $N" 
+        echo -e " $2  $R Failure $N" 
         exit 1
     else
-        echo "$2 is $G Success $N"
+        echo -e "$2 is $G Success $N"
     fi
          
 
