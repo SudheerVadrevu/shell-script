f577328cef58e75594dae27ad64e664cfa82c5c7
Sudheer Vadrevu <vadrevusudheer@gmail.com>
2024-07-18 18:38:49 +0200

    hello-script

---
+++ 11.trial.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudheer="Hi, ra mava this is the first script"
+ #" " ivi ivali manam word ki word ki gap ichinapudu
+echo" This was the first script please read $sudheer"
+echo "the pid of the current script $$ "
+
+./12.secondtrial.sh
+
+echo " After running the other script $sudheer"
---
+++ 12.secondtrial.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudheer="Hi, ra mava this is the second script"
+
+echo "chudam idhi ye variable value ne isthundo $sudheer"
+echo "we will see the process ID for this second script PPID $$"
+
