8836b7a330d887832cfc9b7b98691fdae778a776
Sudheer Vadrevu <vadrevusudheer@gmail.com>
2024-07-23 14:41:31 +0200

    hello-script

--- 13.set.sh
+++ 13.set.sh
@@ -2,7 +2,21 @@
 
 set -e
 
+failure(){
+    echo "Failed at $1: $2"
+}
+
+trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
+
 USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
 
 dnf install mysqqwddddddl -y # idhi kavalani ichamu to check how set -e will encounter it.
 
