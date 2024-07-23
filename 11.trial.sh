3be69ce5a84afb3490d19b51d0f1e8ac76097325
Sudheer Vadrevu <vadrevusudheer@gmail.com>
2024-07-23 14:03:52 +0200

    hello-script

--- 11.trial.sh
+++ 11.trial.sh
@@ -5,10 +5,11 @@ sudheer="Hi, ra mava this is the first script"
 echo "This was the first script please read $sudheer"
 echo "the pid of the current script $$ "
 
-#./12.secondtrial.sh    # ee way lo run chesetapudu manaki kinda vunna variable value change avadu.same
-                        # first script lo variable ki ye value vunte adhe print avutundhi.
-                        # Process ID anedhi different ga vastayi output lo.first di Pid($$) ee script 
-                        # lone di.second di "secondtrial.sh" lone di
+#./12.secondtrial.sh    # Ikkada variable ane kadhu In general ga changes happen in the other script 
+                        # willnot affct by other script ee way lo run chesetapudu manaki kinda vunna 
+                        # variable value change avadu.same first script lo variable ki ye value vunte 
+                        # adhe print Process ID anedhi different ga vastayi output lo.first di Pid($$)  
+                        #  ee script avutundhi lone di.second di "secondtrial.sh" lone di.
 
 source ./12.secondtrial.sh # ee way lo run chese tapudu kinda vunna echo lo variable value change 
                            # ayipothundhi second script lo vunna value ni istundhi.Process Id same ga
---
+++ 13.set.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+USERID=$(id -u)
+
+dnf install mysqqwddddddl -y # idhi kavalani ichamu to check how set -e will encounter it.
+
+dnf install git -y
+
+echo "is script proceeding?"
