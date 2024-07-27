f083ae5230d860a9bbc6fda35ee618bdc784947a
Sudheer Vadrevu <vadrevusudheer@gmail.com>
2024-07-27 19:49:50 +0200

    hello-script

--- practice.sh
+++ practice.sh
@@ -2,6 +2,6 @@
 
 sexywomans=("sindhu" "ramya" "divya")   #size of the array is 3 and index will be start counts from 
                                         # 0,1,2
-echo "first lanja peru ${sexywoman[0]}"
-echo "second lanja peru ${sexywoman[1]}"
-echo "Third lanja peru ${sexywoman[2]}}" 
+echo "first lanja peru ${sexywomans[0]}"
+echo "second lanja peru ${sexywomans[1]}"
+echo "Third lanja peru ${sexywomans[2]}}" 
