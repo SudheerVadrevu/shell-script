5c57daf3a77fed3b7bbecc6eb8794b4c36a5489c
Sudheer Vadrevu <vadrevusudheer@gmail.com>
2024-07-27 19:48:17 +0200

    hello-script

--- practice.sh
+++ practice.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Give me the correct username"
-read -s username
-echo "Give me the password "
-read -s password
-echo "the username is the $username and password is the $password"
+sexywomans=("sindhu" "ramya" "divya")   #size of the array is 3 and index will be start counts from 
+                                        # 0,1,2
+echo "first lanja peru ${sexywoman[0]}"
+echo "second lanja peru ${sexywoman[1]}"
+echo "Third lanja peru ${sexywoman[2]}}" 
