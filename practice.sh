3c78a6f47bd9c9a0442b02bc6186db67a2d4aebe
Sudheer Vadrevu <vadrevusudheer@gmail.com>
2024-07-27 20:11:54 +0200

    hello-script

--- practice.sh
+++ practice.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-echo "all the variables is $@"
-echo "Number of variables is $#"
-echo "Present working directory $PWD"
-echo "Home directory : $HOME"
-echo "The user who was writing this script $USER"
-echo "The name of the script $0"
-echo "Host name is $HOSTNAME"
-echo "the process id of the script $$"
+Sudheer=10
+if [ $Sudheer -ge 30 ]
+then
+    echo "sudheer is intelligent"
+else
+    echo "sudheer is not an intelligent"
+fi
