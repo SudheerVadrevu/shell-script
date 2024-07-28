f6a43b62154416ea77a0fd2182056cb40d117a48
Sudheer Vadrevu <vadrevusudheer@gmail.com>
2024-07-28 15:57:01 +0200

    hello-script

--- practice.sh
+++ practice.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 Sudheer=$(id -u)
+date=$(date+%F-%H-%M-%S)
+vinni=$(echo $0| cut -d "." -f2 )
+logs=/tmp/$date-$vinni.log
+
 sexy()
 {
-if [ $1 -ne 0 ]
-then
-    echo "exit status $1"
-    echo "it was not installed"
-else
-    echo " $2 it was installed"
-fi
+    if [ $1 -ne 0 ]
+    then
+        echo "$1 Some thing error please check the installation"
+    else
+        echo "$2 It was installed"
 }
 if [ $Sudheer -ne 0 ]
 then
